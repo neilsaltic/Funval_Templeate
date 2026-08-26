e72c514bf14e1ca0efbf104061b5f8b6532633d5
neilsaltic <neilsalanocaticona@gmail.com>
2026-08-26 19:53:28 -0400

    middleware con validacion en ZOD

--- src/routes/productos.routes.ts
+++ src/routes/productos.routes.ts
@@ -6,12 +6,13 @@ import {
   postProduct,
   putProduct,
 } from "../controller/product.controller.js";
+import { validateProduct } from "../middleware/product.middleware.js";
 
 export const ProductosRouter: Router = Router();
 
 ProductosRouter.get("/", getProducts);
 ProductosRouter.get("/:id", getProductsById);
-ProductosRouter.post("/", postProduct);
+ProductosRouter.post("/", validateProduct, postProduct);
 ProductosRouter.put("/:id", putProduct);
 ProductosRouter.delete("/:id", deleteProducts);
 
